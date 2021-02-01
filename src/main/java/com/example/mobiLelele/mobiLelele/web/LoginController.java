8e0b526349d97061525af1e0e164282069acdb74
Elizabetaa <elizabetaa99@abv.bg>
2021-02-01 17:00:54 +0200

    Implementation of the user login

--- src/main/java/com/example/mobiLelele/mobiLelele/web/LoginController.java
+++ src/main/java/com/example/mobiLelele/mobiLelele/web/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.mobiLelele.mobiLelele.web;
 
+import com.example.mobiLelele.mobiLelele.model.service.UserLoginServiceModel;
 import com.example.mobiLelele.mobiLelele.security.CurrentUser;
+import com.example.mobiLelele.mobiLelele.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,16 +10,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
 
-    private final CurrentUser currentUser;
+    private final UserService userService;
 
-    public LoginController(CurrentUser currentUser) {
-
-        this.currentUser = currentUser;
+    public LoginController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users/login")
-    public String login(){
+    public String showLogin() {
         return "/auth-login";
+    }
+
+
+    @PostMapping("/users/login")
+    public String login(UserLoginServiceModel userLoginServiceModel) {
+
+        if (userService.authenticate(userLoginServiceModel.getUsername(), userLoginServiceModel.getPassword())) {
+            userService.loginUser(userLoginServiceModel.getUsername());
+            return "redirect:/";
+        } else {
+            return "redirect:/users/login";
+        }
+
     }
 
 }
