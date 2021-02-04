28f86042a1100af5cca8ab41c48f29b84c899883
Elizabetaa <elizabetaa99@abv.bg>
2021-02-04 19:42:39 +0200

    Login validation

--- src/main/java/com/example/mobiLelele/mobiLelele/web/LoginController.java
+++ src/main/java/com/example/mobiLelele/mobiLelele/web/LoginController.java
@@ -4,8 +4,13 @@ import com.example.mobiLelele.mobiLelele.model.service.UserLoginServiceModel;
 import com.example.mobiLelele.mobiLelele.security.CurrentUser;
 import com.example.mobiLelele.mobiLelele.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -16,20 +21,37 @@ public class LoginController {
         this.userService = userService;
     }
 
+    @ModelAttribute("userModel")
+    public UserLoginServiceModel userModel(){
+       return new UserLoginServiceModel();
+    }
+
     @GetMapping("/users/login")
     public String showLogin() {
-        return "/auth-login";
+        return "auth-login";
     }
 
 
     @PostMapping("/users/login")
-    public String login(UserLoginServiceModel userLoginServiceModel) {
+    public String login(@Valid @ModelAttribute UserLoginServiceModel userModel,
+                        BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-        if (userService.authenticate(userLoginServiceModel.getUsername(), userLoginServiceModel.getPassword())) {
-            userService.loginUser(userLoginServiceModel.getUsername());
+        if (bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userModel",userModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel", bindingResult);
+
+            return "redirect:/users/login";
+        }
+
+        if (userService.authenticate(userModel.getUsername(), userModel.getPassword())) {
+            userService.loginUser(userModel.getUsername());
+
             return "redirect:/";
         } else {
-            return "redirect:auth-login";
+            redirectAttributes.addFlashAttribute("userModel",userModel);
+            redirectAttributes.addFlashAttribute("notFound",true);
+
+            return "redirect:/users/login";
         }
 
     }
